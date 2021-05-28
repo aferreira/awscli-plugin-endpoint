5b489b5b7da7e0248faf68e0d513636175179fbb
Adriano Ferreira <a.r.ferreira@gmail.com>
2021-05-27 18:57:02 -0700

    Update __init__.py

--- awscli_plugin_endpoint/__init__.py
+++ awscli_plugin_endpoint/__init__.py
@@ -38,20 +38,18 @@ def get_endpoint_from_profile(profile, command):
 
 def set_endpoint_from_profile(parsed_args, **kwargs):
     endpoint_url = parsed_args.endpoint_url
-    command = parsed_args.command
     # If endpoint set on CLI option, use CLI endpoint
     if endpoint_url is None:
         session = kwargs['session']
         # Set profile to session so we can load profile from config
         if parsed_args.profile:
             session.set_config_variable('profile', parsed_args.profile)
-        service_endpoint = get_endpoint_from_profile(session.get_scoped_config(), command)
+        service_endpoint = get_endpoint_from_profile(session.get_scoped_config(), parsed_args.command)
         if service_endpoint is not None:
             parsed_args.endpoint_url = service_endpoint
 
 def set_verify_from_profile(parsed_args, **kwargs):
     verify_ssl = parsed_args.verify_ssl
-    command = parsed_args.command
     # By default verify_ssl is set to true
     # if --no-verify-ssl is specified, parsed_args.verify_ssl is False
     # so keep it
@@ -60,23 +58,21 @@ def set_verify_from_profile(parsed_args, **kwargs):
         # Set profile to session so we can load profile from config
         if parsed_args.profile:
             session.set_config_variable('profile', parsed_args.profile)
-        service_verify = get_verify_from_profile(session.get_scoped_config(), command)
+        service_verify = get_verify_from_profile(session.get_scoped_config(), parsed_args.command)
         if service_verify is not None:
             parsed_args.verify_ssl = service_verify
             if not service_verify:
                 warnings.filterwarnings('ignore', 'Unverified HTTPS request')
 
 def set_ca_bundle_from_profile(parsed_args, **kwargs):
-    # Respect command line arg if present
-    if parsed_args.ca_bundle is not None:
+    if parsed_args.ca_bundle is not None: # Respect command line arg if present
         return
 
-    command = parsed_args.command
     session = kwargs['session']
     # Set profile to session so we can load profile from config
     if parsed_args.profile:
         session.set_config_variable('profile', parsed_args.profile)
-    parsed_args.ca_bundle = get_ca_bundle_from_profile(session.get_scoped_config(), command)
+    parsed_args.ca_bundle = get_ca_bundle_from_profile(session.get_scoped_config(), parsed_args.command)
 
 def awscli_initialize(cli):
     cli.register('top-level-args-parsed', set_endpoint_from_profile)
