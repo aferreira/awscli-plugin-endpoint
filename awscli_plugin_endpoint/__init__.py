119610b49dcc22493af5ceab282f18d0594cf0d7
Adriano Ferreira <a.r.ferreira@gmail.com>
2021-05-27 20:03:15 -0700

    Update __init__.py

--- awscli_plugin_endpoint/__init__.py
+++ awscli_plugin_endpoint/__init__.py
@@ -19,41 +19,29 @@ def _get_attr_from_profile(profile, command, attr):
         return profile[command][attr]
     return None
 
-def get_endpoint_from_profile(parsed_args, kwargs):
-    return get_attr_from_profile(parsed_args, kwargs, ENDPOINT_URL)
-
 def set_endpoint_from_profile(parsed_args, **kwargs):
     if parsed_args.endpoint_url:   # Respect --endpoint-url if present
         return
     
-    endpoint_url = get_endpoint_from_profile(parsed_args, kwargs)
+    endpoint_url = get_attr_from_profile(parsed_args, kwargs, ENDPOINT_URL)
     if endpoint_url is not None:
         parsed_args.endpoint_url = endpoint_url
-
-def get_verify_from_profile(parsed_args, kwargs):
-    v = get_attr_from_profile(parsed_args, kwargs, VERIFY_SSL)
-    if v is None:
-        return v
-    return str2bool(v)
 
 def set_verify_from_profile(parsed_args, **kwargs):
     if not parsed_args.verify_ssl:   # Respect --no-verify-ssl if present
         return
     
-    verify_ssl = get_verify_from_profile(parsed_args, kwargs)
+    verify_ssl = get_attr_from_profile(parsed_args, kwargs, VERIFY_SSL)
     if verify_ssl is not None:
-        parsed_args.verify_ssl = verify_ssl
-        if not verify_ssl:
+        parsed_args.verify_ssl = str2bool(verify_ssl)
+        if not parsed_args.verify_ssl:
             warnings.filterwarnings('ignore', 'Unverified HTTPS request')
-
-def get_ca_bundle_from_profile(parsed_args, kwargs):
-    return get_attr_from_profile(parsed_args, kwargs, CA_BUNDLE)
 
 def set_ca_bundle_from_profile(parsed_args, **kwargs):
     if parsed_args.ca_bundle:   # Respect --ca-bundle if present
         return
 
-    ca_bundle = get_ca_bundle_from_profile(parsed_args, kwargs)
+    ca_bundle = get_attr_from_profile(parsed_args, kwargs, CA_BUNDLE)
     if ca_bundle is not None:
         parsed_args.ca_bundle = ca_bundle
 
